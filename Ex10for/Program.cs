ad68bcfa77b568112c8247e69366307a5faf5687
Arthur Foschiani De Souza <43127523890@info.local>
2019-04-08 08:12:44 -0300

    começo aula segunda

--- Ex10for/Program.cs
+++ Ex10for/Program.cs
@@ -6,15 +6,19 @@ namespace Ex10for
     {
         static void Main(string[] args)
         {
-            int piramides = 10;
-            int num = 10;
+            int piramides = 0;
+            int num = 0;
             int i;
             int j;
-            string a = "";
+
+            Console.WriteLine("Quantos pontos você deseja ter na base da piramide?");
+            piramides = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quantas piramides você deseja ter?");
+            num = int.Parse(Console.ReadLine());
 
             for (i = 1; i <= piramides; i++)
             {
-                a = "";
+                string a = "";
                 for (j = 1; j <= num; j++)
                 {
                     a += "*";
