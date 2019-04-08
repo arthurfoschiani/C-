e1cfb01a2659342de2e89243898faeb3cc1f96f8
Arthur Foschiani De Souza <43127523890@info.local>
2019-04-08 07:43:27 -0300

    Começo aula segunda

---
+++ Ex10for/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex10for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int piramides = 10;
+            int num = 10;
+            int i;
+            int j;
+            string a = "";
+
+            for (i = 1; i <= piramides; i++)
+            {
+                a = "";
+                for (j = 1; j <= num; j++)
+                {
+                    a += "*";
+                    Console.WriteLine(a);
+                }
+            }
+        }
+    }
+}
