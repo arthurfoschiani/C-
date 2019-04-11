f0e3056545c0b98821128a8dbadcb005fb269bb3
Arthur Foschiani De Souza <43127523890@info.local>
2019-04-11 11:28:15 -0300

    Aula Quinta

--- GeradorAbobrinha/Program.cs
+++ GeradorAbobrinha/Program.cs
@@ -1,23 +1,55 @@
 ﻿using System;
 
-namespace GeradorAbobrinha {
-    class Program {
-        static void Main (string[] args) {
-            System.Console.WriteLine ("Escreve uma palavra aí, meu consagrado!");
+namespace GeradorAbobrinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Escreve uma palavra aí, meu consagrado!");
 
             int maxPalavrasUsuario = 5;
-            int maxPalavrasFrase = 8;
+            int maxPalavrasFrase = 5;
 
             string[] palavrasUsuario = new string[maxPalavrasUsuario];
 
-            for (int i = 0; maxPalavrasUsuario > 0; i++) {
-                palavrasUsuario[i] = Console.ReadLine ();
-                if (--maxPalavrasUsuario != 0) {
-                    System.Console.WriteLine ($"Faltam {maxPalavrasUsuario}. Digite mais uma!");
-                } else {
-                    System.Console.WriteLine ("Valeu, Jovem! Agora deixa comigo!");
+            for (int i = 0; maxPalavrasUsuario > 0; i++)
+            {
+                palavrasUsuario[i] = Console.ReadLine();
+                if (--maxPalavrasUsuario != 0)
+                {
+                    System.Console.WriteLine($"Faltam {maxPalavrasUsuario}. Digite mais uma!");
+                }
+                else
+                {
+                    System.Console.WriteLine("Valeu, Jovem! Agora deixa comigo!");
                 }
             }
+            string[,] matrizPalavras = { { "eu", "ele", "ela", "nós", "" },
+                                         { "fui", "foi", "estavamos", "estava", "" },
+                                         { "viajar", "tomar", "andar", "mexer", "" },
+                                         { "para", "banho", "eperando", "no seu", "" },
+                                         { "praia", "escondido", "a chuva", "filho", "" } };
+            for (int i = 0; i < matrizPalavras.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrizPalavras.GetLength(0); j++)
+                {
+                    if ("".Equals(matrizPalavras[i, j]))
+                    {
+                        matrizPalavras[i, j] = palavrasUsuario[i];
+                    }
+                }
+            }
+
+            string frase = "";
+
+            Random r = new Random();
+            for (int i = 0; i < maxPalavrasFrase; i++)
+            {
+                frase += matrizPalavras[i, r.Next(matrizPalavras.GetLength(0))]
+                    + " ";
+            }
+            System.Console.WriteLine("Sua frase é: \n" + frase);
         }
     }
 }
---
+++ MediaArray/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace MediaArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nome = new string[10];
+            string[] sobrenome = new string[10];
+            double[] nota1 = new double[10];
+            double[] nota2 = new double[10];
+            double[] media = new double[10];
+            string[] situacao = new string[10];
+
+            System.Console.WriteLine("--------Relatório Escolar--------");
+
+            for (int i = 0; i < nome.Length; i++)
+            {
+                System.Console.WriteLine($"Insira o nome do {i + 1}º aluno:");
+                nome[i] = Console.ReadLine();
+
+                System.Console.WriteLine($"Insira o sobrenome do {i + 1}º aluno");
+                sobrenome[i] = Console.ReadLine();
+
+                System.Console.WriteLine($"Insira a primeira nota do {i + 1}º aluno:");
+                nota1[i] = double.Parse(Console.ReadLine());
+
+                System.Console.WriteLine($"Insira a segunda nota do {i + 1}º aluno:");
+                if(double.TryParse(Console.ReadLine(), out nota2[i])) {
+
+                        if (nota1[i] > 100 || nota2[i] > 100 || nota1[i] < 1 || nota2[i] < 1)
+                        {
+                            System.Console.WriteLine("Desculpe, nota(s) inválida(s)");
+                            break;
+                        }
+
+                        media[i] = (nota1[i] + nota2[i]) / 2;
+
+                        if (media[i] >= 50)
+                        {
+                            situacao[i]="Aprovado";
+                        }
+                        else
+                        {
+                            situacao[i]="Reprovado";
+                        }
+                }
+                else{
+                    System.Console.WriteLine("Você digitou palavras. Só é aceito números.");
+                }
+            }
+
+            for (int i = 0; i < nome.Length; i++)
+            {
+                System.Console.WriteLine($"O aluno {nome[i]} {sobrenome[i]} fechou com a média {media[i]} e está {situacao[i]}");
+            }
+}
+    }
+}
